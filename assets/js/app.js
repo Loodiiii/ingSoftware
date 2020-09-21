815e9a8653f66d40d6f372c5207736d5cabf8858
Loodiiii <pnovitovic1@gmail.com>
2020-09-21 16:41:12 +0200

    smooth-scroll with improved pop-up

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+//Declaring variables
 const hamburger = document.querySelector('.hamburger')
 const list = document.querySelector('.list')
 const cross = document.querySelector('.cross')
@@ -6,7 +7,16 @@ const hr1 = document.querySelector('.hr-1')
 const hr2 = document.querySelector('.hr-2')
 const hr3 = document.querySelector('.hr-3')
 
+//Event Listeners
+
 hamburger.addEventListener('click', function () {
+    hr1.classList.toggle('hr-1-toggle')
+    hr2.classList.toggle('hr-2-toggle')
+    hr3.classList.toggle('hr-3-toggle')
+    list.classList.toggle('block')
+})
+
+list.addEventListener("click", function () {
     hr1.classList.toggle('hr-1-toggle')
     hr2.classList.toggle('hr-2-toggle')
     hr3.classList.toggle('hr-3-toggle')
